a69dd3343c78f3df6b14687e92b77a1cdaf3e17a
Annie Yin <anniesy2@uci.edu>
2026-06-23 14:37:32 -0700

    feat: post function for admin authentication

---
+++ app/api/auth/route.ts
@@ -0,0 +1,29 @@
+import { cookies } from "next/headers";
+
+export async function POST(request: Request): Promise<Response> {
+    const body = await request.json();
+    const entered_pw = body.entered_pw;
+
+    if (entered_pw === process.env.ADMIN_PASSWORD) {
+        const cookie = await cookies();
+        cookie.set("doggonaut", process.env.AUTH_SECRET!, {
+            httpOnly: true,
+            path: "/"
+        });
+
+        return new Response(JSON.stringify({message: 'Login success.'}), {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+    }
+    else {
+        return new Response(JSON.stringify({ message: 'Login fail.' }), {
+            status: 401,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    }
+}
