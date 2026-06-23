ccced39409bd84953d3695d22a3fc85be29870ef
Annie Yin <anniesy2@uci.edu>
2026-06-23 16:22:01 -0700

    feat: get list of sections

---
+++ lib/sections.ts
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import matter from 'gray-matter';
+import path from 'path';
+
+export default function getSections(chapterSlug: string) {
+    const sections = fs.readdirSync(path.join(process.cwd(), 'content', chapterSlug));
+    return (sections.map(section => {
+        const fp = path.join(process.cwd(), 'content', chapterSlug, section);
+        const fileContents = fs.readFileSync(fp, 'utf8');
+        const {data} = matter(fileContents);
+        return ({
+            slug: section,
+            title: data.title,
+            section: data.section,
+            description: data.description,
+            published: data.published,
+            lastEdited: data.lastEdited
+        });
+    }))    
+}
