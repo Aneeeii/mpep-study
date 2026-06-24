fa9aaa9bdd6065e138ba5fb56857665e9427714b
Annie Yin <anniesy2@uci.edu>
2026-06-23 23:04:40 -0700

    feat: update to main page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,10 @@ export default function Home() {
   const chapters = getChapters();
   return (
     <>
+    <h1>Hello</h1>
+    <h2>Recent Updates</h2>
+    <p>06/23/2026 - Setting up foundations :)</p>
+    <h2>Chapters</h2>
       {chapters.map(chapter => (
         <Link key={chapter.slug} href={`/chapters/${chapter.slug}`}>Chapter {chapter.number}: {chapter.title}</Link>
       ))}
